c44aa00242ba123decb312bb89ea00234e31d111
Naveen <koppisettinaveen1432@gmail.com>
2026-08-06 19:56:19 +0530

    Configured environment variables for database

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -1,22 +1,35 @@
 package database;
 
 import java.sql.*;
-public class DBConnection {
-	
-	protected Connection con;
-	
-	public DBConnection() {
-		String url = "jdbc:mysql://localhost:3306/StudentManagementSystem";
-		String username = "root";
-		String password = "naveen";
-		
-		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url,username,password);
-			System.out.println("Connection Succesfully");
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-	}
 
+public class DBConnection {
+
+    protected Connection con;
+
+    public DBConnection() {
+
+        String url = System.getenv().getOrDefault(
+                "DB_URL",
+                "jdbc:mysql://mysql-ca81bda-studentmanagementsystem29.d.aivencloud.com:24996/studentmanagementsystem?sslMode=REQUIRED"
+        );
+
+        String username = System.getenv().getOrDefault(
+                "DB_USERNAME",
+                "avnadmin"
+        );
+
+        String password = System.getenv("DB_PASSWORD");
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(url, username, password);
+
+            System.out.println("Connected Successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
